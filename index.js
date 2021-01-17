4b6dbfad329f345f0b1293c973d5135180687f50
Jordan Rendall <thomasjrendall@gmail.com>
2021-01-17 11:48:16 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    return server.listen({ port: process.env.PORT || 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
